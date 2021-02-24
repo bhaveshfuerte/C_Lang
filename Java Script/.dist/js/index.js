43a1bdd1b6bae8ea559d149ad8e95346153c7109
BHAVESH <bhaveshfuerte@gmail.com>
2021-02-24 17:49:05 +0530

    Java Script

---
+++ Java Script/.dist/js/index.js
@@ -0,0 +1,8 @@
+console.time('your time is');
+console.log("Smarty");
+console.log(80000*9000);
+console.log([20,25,27,80]);
+console.table({Name: 'smarty',city: 'junagadh'});
+console.warn('I am Smarty Understand')
+console.timeEnd('your time is');
+console.assert(age>199, 'age age age not >');    
